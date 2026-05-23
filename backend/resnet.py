3cb0f7928f0865e0646fc29461c98aca2dd69887
Amelie Jehanne Lanto <alant044@uottawa.ca>
2026-05-23 14:57:28 -0400

    dev: Test analyse d'image

--- backend/resnet.py
+++ backend/resnet.py
@@ -19,3 +19,36 @@ class ImageToSoundResNet(nn.Module):
         x = self.features(x)
         audio_params = self.audio_projection(x)
         return audio_params
+
+import torchvision.transforms as transforms
+from PIL import Image
+
+# Preprocessing
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
+
+# La fonction que tout le monde va appeler
+def analyze_image(image_path: str) -> dict:
+    model = ImageToSoundResNet()
+    model.eval()
+
+    img = Image.open(image_path).convert("RGB")
+    tensor = transform(img).unsqueeze(0)
+
+    with torch.no_grad():
+        params = model(tensor).squeeze().tolist()
+
+    return {
+        "harmony_base":    params[0],
+        "harmony_mid":     params[1],
+        "harmony_high":    params[2],
+        "texture_density": params[3],
+        "brightness":      params[4],
+        "filter_cutoff":   params[5],
+        "filter_resonance":params[6],
+        "reverb":          params[7],
+        "delay":           params[8],
+        "complexity":      params[9],
+    }
---
+++ test.py
@@ -0,0 +1,4 @@
+from backend.resnet import analyze_image
+
+result = analyze_image("test.jpg")
+print(result)
